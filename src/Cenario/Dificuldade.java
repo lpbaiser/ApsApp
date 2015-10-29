f2207c6a171724b02e998b5532a23e22420d00f1
Leonardo <lpbaiser@gmail.com>
2015-10-29 17:43:06 -0200

    sdfd

--- src/Cenario/Dificuldade.java
+++ src/Cenario/Dificuldade.java
@@ -7,46 +7,43 @@ import java.util.Collections;
 
 import javax.swing.JButton;
 
-public class Dificuldade{
-	
+public class Dificuldade {
+
 	JButton[][] vBtn;
 	int nLinhas;
 	int nColunas;
 
 	ActionListener actionL = new ActionListener() {
-		
+
 		@Override
 		public void actionPerformed(ActionEvent e) {
-			
+
 			String nome = e.getActionCommand();
 			System.out.println(nome);
-			
+
 		}
 	};
-	
-	public JButton[][] geraVetorJButton(ArrayList<Integer>numeros) {
-		vBtn = new JButton[nLinhas][nColunas];
-		
-		
-		
-		for (int i = 0; i < (nLinhas); i++) {
-			for (int j = 0; j < nColunas; j++) {
+
+	public JButton[][] geraVetorJButton(ArrayList<Integer> numeros) {
+		vBtn = new JButton[this.nLinhas][this.nColunas];
+		int aux = 0;
+		for (int i = 0; i < (this.nLinhas); i++) {
+			for (int j = 0; j < this.nColunas; j++) {
 				JButton b = new JButton();
-				b.addActionListener(actionL);
-				b.setActionCommand(String.valueOf(i));
+				b.addActionListener(this.actionL);
+				b.setActionCommand(String.valueOf(numeros.get(aux)));
+				b.setEnabled(false);
 				vBtn[i][j] = b;
+				aux++;
 			}
-			
-		}	
-		
+		}
+
 		return vBtn;
 	}
 
-	
 	public ArrayList<Integer> random(int tamanho) {
 		ArrayList<Integer> numeros = new ArrayList<>();
 		for (int i = 0, j = 0; i < (tamanho - 1); i += 2) {
-
 			numeros.add(j);
 			numeros.add(j);
 			j++;
@@ -54,6 +51,38 @@ public class Dificuldade{
 		Collections.shuffle(numeros);
 
 		return numeros;
+	}
+
+	public JButton[][] getvBtn() {
+		return vBtn;
+	}
+
+	public void setvBtn(JButton[][] vBtn) {
+		this.vBtn = vBtn;
+	}
+
+	public int getnLinhas() {
+		return nLinhas;
+	}
+
+	public void setnLinhas(int nLinhas) {
+		this.nLinhas = nLinhas;
+	}
+
+	public int getnColunas() {
+		return nColunas;
+	}
+
+	public void setnColunas(int nColunas) {
+		this.nColunas = nColunas;
+	}
+
+	public ActionListener getActionL() {
+		return actionL;
+	}
+
+	public void setActionL(ActionListener actionL) {
+		this.actionL = actionL;
 	}
 
 }
--- src/Cenario/GUI.java
+++ src/Cenario/GUI.java
@@ -26,16 +26,24 @@ public class GUI extends JFrame implements ActionListener {
 	public GUI() {
 		setTitle("JOGO DA MEMORIA");
 		setSize(600, 600);
-		cbDificuldade.addItem(item);
+		
+		cbDificuldade.addItem("12 Peças");
+		cbDificuldade.addItem("24 Peças");
+		cbDificuldade.addItem("36 Peças");
+		
 		// cp = getContentPane();
 
 		numeros = d.random(12);
+		d.setnColunas(4);
+		d.setnLinhas(3);
 		vBtn = d.geraVetorJButton(numeros);
+		
 		j = geraCenario(vBtn, 3, 4);
 		al = d.actionL;
 
 		// cp.setLayout(new BorderLayout());
 		// cp.add(j, BorderLayout.CENTER);
+		add(cbDificuldade);
 		add(j);
 		
 //		vBtn[0][0].addActionListener(new ActionListener() {
@@ -59,12 +67,15 @@ public class GUI extends JFrame implements ActionListener {
 	}
 
 
+
+
 	public JPanel geraCenario(JButton[][] vBtn, int nLinha, int nColuna) {
 		JPanel panel = new JPanel();
-		GridLayout layout = new GridLayout(0, nColuna);
+		GridLayout layout = new GridLayout(nLinha, nColuna);
 		panel.setLayout(layout);
 		for (int i = 0; i < nLinha; i++) {
 			for (int j = 0; j < nColuna; j++) {
+				System.out.println("i: " + i+ " j: "+j);
 				panel.add(vBtn[i][j]);
 			}
 		}
